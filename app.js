fc912c0c709760d1255a6ab850c6b072bf4a7e12
Francis Avancena <afrancisoliver@yahoo.com>
2018-04-27 20:21:56 +0800

    Added new branch

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
 const bodyParser = require('body-parser');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('db.sqlite3');
 const app = express();
 const port = process.env.PORT || 8000;
-var request = require('request');
+const request = require('request');
 
 // SQLite3 setup
 db.serialize(() => {
@@ -28,6 +30,9 @@ db.serialize(() => {
 });
 
 // Middleware
+app.use(cookieParser());
+const csrfProtection = csrf({ cookie: true });
+const parseForm = bodyParser.urlencoded({extended:false});
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
@@ -43,23 +48,9 @@ app.get('/', (req, res, next)=>{
 	});
 });
 
-app.get('/new', (req, res, next)=>{
-	res.render('newpost');
+app.get('/new', csrfProtection, (req, res)=>{
+	res.render('newpost', { csrfToken: req.csrfToken() });
 });
-
-app.post('/new', (req, res, next)=>{
-	var newpost = {
-		title: req.body.title,
-		body: req.body.body
-	};
-	request.post('http://localhost:8000/posts', {json: newpost},
-		(err, req, bod) => {
-			console.log(err);
-		}
-	);
-	res.redirect('/posts');
-});
-
 
 app.get('/posts', (req, res, next) => {
 	db.all("SELECT * FROM posts", function(err, rows) {
@@ -67,14 +58,14 @@ app.get('/posts', (req, res, next) => {
 	});
 });
 
-app.post('/posts', (req, res, next) => {
+app.post('/posts', parseForm, csrfProtection, (req, res) => {
 	console.log(req.body);
 	db.run("INSERT INTO posts(title, body) VALUES (?, ?)", [req.body.title, req.body.body]);
-	res.redirect('/posts');		
+	res.redirect('/posts');
 	
 });
 
-app.get('/posts/:id', (req, res, next) => {
+app.get('/posts/:id', (req, res) => {
 	console.log(req.params.id);	
 	db.get("SELECT * FROM posts WHERE id = ?", [req.params.id], function(err, row){
 		console.log(row);
@@ -82,29 +73,21 @@ app.get('/posts/:id', (req, res, next) => {
 	});
 });
 
-app.post('/posts/:id', (req, res, next) => {
+app.post('/posts/:id', (req, res) => {
 	db.run("UPDATE posts SET title = ?, body = ? WHERE id = ?", [req.body.title, req.body.body, req.params.id]);
 	res.redirect(`/posts/${req.params.id}`);
 });
 
-app.get('/edit/:id', (req, res, next)=>{
+app.post('/posts/:id/delete', (req, res) => {
+	db.run("DELETE posts WHERE id = ?", [req.params.id]);
+	res.redirect('/posts');
+});
+
+app.get('/edit/:id', (req, res)=>{
 	db.get("SELECT * FROM posts WHERE id = ?", [req.params.id], function(err, row){
 		console.log(row);
 		res.render('editpost', {post: row});
 	});
-});
-
-app.post('/edit/:id', (req, res, next) => {
-	var newpost = {
-		title: req.body.title,
-		body: req.body.body
-	};
-	request.post(`http://localhost:8000/posts/${req.params.id}`, {json: newpost},
-		(err, req, bod) => {
-			console.log(err);
-		}
-	);
-	res.redirect(`/posts/${req.params.id}`);
 }); 
 
 // Listen
